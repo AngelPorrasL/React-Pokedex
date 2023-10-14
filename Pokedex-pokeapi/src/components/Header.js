73660b826c1adf7ca01762a47d38d35b2b3e31a9
AngelPorrasL <angelporras799@gmail.com>
2023-10-13 18:27:51 -0600

    Add project files.

---
+++ Pokedex-pokeapi/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>PokeDex</h1>
+      <p>
+        Para ver los pokemones disponibles por el momento usa las flechas de la pokedex
+      </p>
+    </header>
+  );
+}
